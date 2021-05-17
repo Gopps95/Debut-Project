fb02707073c9a4b35d996322dfd91cfbcd90f6f1
Gopps95 <84285449+Gopps95@users.noreply.github.com>
2021-05-17 20:27:43 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,12 @@
+function purple_btn(){
+    document.body.style.background="purple"
+
+}
+function green_btn(){
+    document.body.style.background="green"
+
+}
+function yellow_btn(){
+    document.body.style.background="grey"
+
+}
